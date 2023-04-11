610ec46e12d1991c1a5f9996dadab1a5b671a909
Denzi0 <darkblackx44@gmail.com>
2023-04-11 10:36:15 +0800

    added resusable-css and footer

--- src/pages/Home/Sections/Footer.jsx
+++ src/pages/Home/Sections/Footer.jsx
@@ -1,12 +1,10 @@
 import "./Footer.css";
 function Footer() {
   return (
-    <footer className="footer-section">
+    <footer className="footer-section flex-container">
       <div className="container">
-        <div className="col-lg-12">
-          <div className="footer-content">
-            <p>© 2020 All rights reserved.</p>
-          </div>
+        <div className="footer-content">
+          <p>© 2020 All rights reserved.</p>
         </div>
       </div>
     </footer>
